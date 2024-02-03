420c1e209ec49d0ec8f589f28d5651c86f9e2e6a
edoardorizzo <edo96.rizzo@gmail.com>
2024-02-03 19:27:39 +0100

    install redux

---
+++ src/js/reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    users: [],
+  };
+  
+  const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'SET_USERS':
+        return {
+          ...state,
+          users: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
+  
