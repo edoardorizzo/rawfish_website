a1d30c08b340579098673097c7ea132ea4119840
edoardorizzo <edo96.rizzo@gmail.com>
2024-02-05 15:46:25 +0100

    fix sidebar component

--- src/components/SearchBarComponent.jsx
+++ src/components/SearchBarComponent.jsx
@@ -7,12 +7,6 @@ function SearchBarComponent({ onSearch }) {
 
   return (
     <div className="search_bar">
-      <div className="site_logo">
-        <div className="site_image">
-          <img src="/rawfish-logo.png" alt="" />
-        </div>
-        <label>RAWFISH FINDER</label>
-      </div>
       <p>Find the people you want by searching for their first name, last name, city, or state.</p>
       <input type="text" placeholder="Search..." onChange={handleSearch} />
     </div>
--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -1,14 +1,19 @@
-import SearchBarComponent from "./SearchBarComponent"
+import SearchBarComponent from "./SearchBarComponent";
 
-function SidebarComponent({onSearch}) {
-    return(
-        <>
-            <div className="sidebar">
-                <SearchBarComponent onSearch={onSearch}/>
-            </div>
-        </>
-    )
+function SidebarComponent({ onSearch }) {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="site_logo">
+          <div className="site_image">
+            <img src="/rawfish-logo.png" alt="" />
+          </div>
+          <label>RAWFISH FINDER</label>
+        </div>
+        <SearchBarComponent onSearch={onSearch} />
+      </div>
+    </>
+  );
 }
 
-
-export default SidebarComponent
+export default SidebarComponent;
